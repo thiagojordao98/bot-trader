465949d01672eec8b0f8c9e268e9d79d8d6465b6
Thiago Jordão <thiagojordao98@gmail.com>
2022-04-12 23:50:26 -0300

    btc-usd monitor

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("olá bot")
+  async function processBtcUsd() {
+  const axios = require('axios');
+  const response = await axios.get(
+    'https://api.binance.com/api/v3/klines?symbol=BTCBUSD&interval=1m',
+  );
+  const candle = response.data[499];
+  const price = parseFloat(candle[4]);
+  if (price >= 42195) {
+    console.log('Sell');
+  } else if (price <= 41000) {
+    console.log('buy');
+  } else console.log('Wait...');
+  console.log(candle[4]);
+}
+
+async function processEthUsd() {
+  const axios = require('axios');
+  const response = await axios.get(
+    'https://api.binance.com/api/v3/klines?symbol=ETHBUSD&interval=1m',
+  );
+  const candle = response.data[499];
+  const price = parseFloat(candle[4]);
+  if (price >= 3185) {
+    console.log('Sell');
+  } else if (price <= 3100) {
+    console.log('Buy');
+  } else console.log('Wait...');
+  console.log(candle[4]);
+}
+
+setInterval(processBtcUsd, 1000);
+setInterval(processEthUsd, 1000);
+
+processBtcUsd();
+processEthUsd();
+
